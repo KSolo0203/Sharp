5b7fe181a5e235234610d0e6e42cae62ce50ae72
KSolo0203 <jr.solovich@gmail.com>
2022-08-28 19:02:36 +0300

    Serpinsky triangle

---
+++ Example005/Program.cs
@@ -0,0 +1,38 @@
+﻿Console.Clear();
+// Console.SetCursorPosition(10,4);
+// Console.WriteLine(+);
+int xa = 1, ya =25, 
+    xb = 35, yb = 1,
+    xc = 70, yc = 25;
+
+Console.SetCursorPosition(xa,ya);
+Console.Write("+");
+Console.SetCursorPosition(xb,yb);
+Console.Write("+");
+Console.SetCursorPosition(xc,yc);
+Console.Write("+");
+
+int x = xa, y = ya, i = 0;
+
+while (i < 15000)
+{
+    int what = new Random().Next(0,3);
+    if (what == 0)
+    {
+        x = (x+xa)/2;
+        y = (y+ya)/2;
+    }
+    if (what == 1)
+    {
+        x = (x+xb)/2;
+        y = (y+yb)/2;
+    }
+    if (what == 2)
+    {
+        x = (x+xc)/2;
+        y = (y+yc)/2;
+    }    
+    Console.SetCursorPosition(x,y);
+    Console.Write("+");
+    i++;
+}
